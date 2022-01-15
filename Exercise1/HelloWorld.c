52a1f57e372e18e8246164ff43c1510dc9fec146
benevolentimp <jaakkola.jooa@gmail.com>
2022-01-15 22:03:33 +0200

    Initial HelloWorld commit

---
+++ Exercise1/HelloWorld.c
@@ -0,0 +1,26 @@
+/*
+Title:      HelloWorld-program in C
+Auhtor:     Jooa Jaakkola
+Desc:       Just "printf" the string "Hello, World!!!" (with a linebreak) 
+            within a necessary main()-function
+*/
+
+#include <stdio.h> // importing libraries (stdio.h for printf); '#' starts "prepocessor" commands
+
+int main() {
+
+    printf("Hello, World!!!\n"); // sentences end with semicolon; functions end with curlies
+    
+    return 0; // non void functions return always something!!! 0 == nothing special
+    
+}
+
+/*
+gcc (calls compiler)
+-Wall (compiler option to show all warnings)
+-Wextra (compiler option for more warnings)
+-std=c11 (compiler option for)
+HelloWorld.c (target file)
+-o (flag)
+HelloWorld (compiled "executable")
+*/
