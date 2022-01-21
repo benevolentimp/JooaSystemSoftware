7efad3619c891ff4349d99e807032035ad701f99
benevolentimp <jaakkola.jooa@gmail.com>
2022-01-21 12:44:54 +0200

    Exercise_2 4c

--- Exercise2/Exercise2.c
+++ Exercise2/Exercise2.c
@@ -5,12 +5,21 @@ Desc:
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 const char* comparison(int fI, int sI);
+int sum(int fI, int sI);
+int diff(int fI, int sI);
+int pro(int fI, int sI);
+double divi(int fI, int sI);
+double mean(int fI, int sI);
+
 
 int main(void) {	
 
-	int firstInteger;
-	int secondInteger;
+	int firstInteger, secondInteger;
+	int summa, erotus, tulo, pot;
+	double jako, kA;
 	
 	printf("Give an integer: ");
 	scanf("%d", &firstInteger); //goes right after the printf!!!
@@ -31,12 +40,81 @@ int main(void) {
 		printf("Your integers are %d and %d\n\n", firstInteger, secondInteger);
 	}
 
-	printf("%s\n", comparison(firstInteger, secondInteger)); //function call and giving it the parameter values
+
+	printf("%s", comparison(firstInteger, secondInteger)); //function call and giving it the parameter values
 	
+	
+	summa = sum(firstInteger, secondInteger);
+	printf("The sum of your integers is %d\n", summa);
+	
+	erotus = diff(firstInteger, secondInteger);
+	printf("The difference between your integers is %d\n", erotus);
+	
+	tulo = pro(firstInteger, secondInteger);
+	printf("The product of your integers is %d\n", tulo);
+	
+	jako = divi(firstInteger, secondInteger);
+	printf("The division of your integers is %.2f\n", jako);
+	
+	kA = mean(firstInteger, secondInteger);
+	printf("The average between your two integers is %.2f\n", kA);
+	
+	pot = pow(firstInteger, secondInteger);
+	printf("Your first number to the power of the second one is %d\n", pot);
 	
 	return 0;
 }
 
+int sum(int fI, int sI) {
+	
+	int resultSum;
+	
+	resultSum = fI + sI;
+	
+	return resultSum;
+}
+
+int diff(int fI, int sI) {
+	int resultDiff;
+	
+	if (fI > sI) {
+		resultDiff = fI - sI;
+		return resultDiff;
+	} else if (sI > fI) {
+		resultDiff = sI - fI;
+		return resultDiff;
+	} else if (fI == sI) {
+		return 0;
+	}
+}
+
+int pro(int fI, int sI) {
+	int resultPro;
+	
+	resultPro = fI * sI;
+	return resultPro;
+}
+
+double divi(int fI, int sI) {
+	double resultDivi;
+	if (sI == 0) {
+		fprintf(stderr, "ZeroDivisionError, exiting execution...\n"); // handling zero division with a library
+		exit(-1);
+	} else {
+		resultDivi = (double) fI / (double) sI; // type-casting "passes" into correct type
+		return resultDivi;
+	}
+}
+
+double mean(int fI, int sI) {
+	double resultMean;
+	resultMean = ((double) fI + (double) sI) / 2;
+	return resultMean;
+}
+
+
+/* Special function for if-else -block comparing given inputs,
+   printing out a message for each clause. (4b) */
 const char* comparison(int fI, int sI) {
 	
 	if (fI > sI) {
