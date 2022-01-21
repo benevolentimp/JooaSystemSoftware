3fdb7814147ea3c46e2c6e453816fe0ef27e1000
benevolentimp <jaakkola.jooa@gmail.com>
2022-01-21 09:59:21 +0200

    Exercise_2 4b

--- Exercise2/Exercise2.c
+++ Exercise2/Exercise2.c
@@ -8,6 +8,7 @@ Desc:
 
 
 int main() {	
+
 	int firstInteger;
 	int secondInteger;
 	
@@ -17,8 +18,18 @@ int main() {
 	printf("Give another integer: ");
 	scanf("%d", &secondInteger);
 	
+	printf("First inputted integer is %d\nand the second one %d\n\n", firstInteger, secondInteger);
 	
-	printf("First inputted integer is %d\nand the second one %d\n", firstInteger, secondInteger);
+	
+	if (firstInteger > secondInteger) {
+		printf("Your first input %d is bigger than the second one %d\n\n", firstInteger, secondInteger);
+	} 
+	if (secondInteger > firstInteger){
+		printf("Your second input %d is bigger than the first one %d\n\n", secondInteger, firstInteger);
+	} else if (firstInteger == secondInteger){
+		printf("Both of your inputs are identical, yay!!!\n");
+		printf("%d == %d\n\n", firstInteger, secondInteger);
+	}
 	
 	return 0;
 }
