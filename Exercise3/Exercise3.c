c1a13c79bdb7bbf55a42d122a562ea3d9df0926f
benevolentimp <jaakkola.jooa@gmail.com>
2022-01-30 14:17:55 +0200

    Exercise_3 3b

--- Exercise3/Exercise3.c
+++ Exercise3/Exercise3.c
@@ -1,33 +1,59 @@
 /*
 Title:      Exercise3.c - Selection and iteration
 Auhtor:     Jooa Jaakkola
-Desc:       
-*/
+Desc:       Programming basics -practise with arithmetic progression etc.
+*/////////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
 
 int apLoop();
+int apLoopEven();
 
 
 int main() {
 
-    printf("The sum of arithmetic progression of one from 1 to 10 is %d.\n\n", apLoop());
+    printf("The sum of arithmetic progression of one from 1 to 100 is %d.\n\n", apLoop());
+    
+    printf("The sum of arithmetic progression of even numbers from 1 to 1000 is %d.\n\n", apLoopEven());
     
     return 0;
 }
 
 
+/*
+Arithmetic Progression:
+*///////////////////////
 int apLoop() {
 
     int result = 0;
 	
-	for (int i = 1; i < 11; i++) {
+	for (int i = 1; i < 101; i++) {
         
         result = result + i;
 	}
 	
 	return result;
 }
+
+int apLoopEven() {
+
+	int result = 0;
+	
+	for (int i = 1; i < 1001; i++) {
+        
+        if (i % 2 != 0) { // Iterate through each even number, EXCEPT when number is odd (replaces "continue").
+        
+        	i += 1;
+        	
+        }
+        
+        result = result + i;
+     
+	}
+	
+	return result;
+}
+
 
 /*
 eof
