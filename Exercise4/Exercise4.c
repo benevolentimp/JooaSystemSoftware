fb403225de2dc69c62cb63498492c5e527868e6c
benevolentimp <jaakkola.jooa@gmail.com>
2022-02-06 14:37:51 +0200

    Exercise_4 3a

---
+++ Exercise4/Exercise4.c
@@ -0,0 +1,71 @@
+/*
+Title:      Exercise4.c - Basics of Functions
+Auhtor:     Jooa Jaakkola
+Desc:       Programming basics - Learning to divide code into "function-blocks"
+*/////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+/*
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h> // For seeing the max (integer) value
+#include <math.h>
+*/
+
+int countNegatives();
+
+
+int main() {
+	
+	int countedNegatives = countNegatives();
+	
+	if (countedNegatives == 1) {
+	
+		printf("You gave %d negative number.\n", countedNegatives);
+		
+	} else if (countedNegatives == 0) {
+	
+		printf("You gave no negative numbers.\n");
+		
+	} else {
+	
+		printf("You gave %d negative numbers.\n", countedNegatives);
+	}
+    
+    
+    return 0;
+}
+
+int countNegatives() {
+
+	int inputs = 0;
+	int negatives = 0;
+	int local_iterator = 1;
+	
+	while (local_iterator) {
+		
+		printf("Give an integer (Press 0 to exit): "); scanf("%d", &inputs);
+		
+		if (inputs == 0) {
+			
+			printf("Okay... exiting program...\n\n");
+			local_iterator = 0;
+			
+		} else if (inputs < 0) {
+			
+			//printf(">>> %d\n", inputs);
+			negatives++;
+			
+		} else {
+			
+			//printf(">>> %d\n", inputs);
+		}
+	}
+	
+	return negatives;
+}
+
+
+/*
+eof
+*/
