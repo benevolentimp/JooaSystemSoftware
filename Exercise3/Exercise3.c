d7f0e1619387113c397a3a35fe55dc46fa1c96a0
benevolentimp <jaakkola.jooa@gmail.com>
2022-02-06 13:26:03 +0200

    Exercise_3 Return

--- Exercise3/Exercise3.c
+++ Exercise3/Exercise3.c
@@ -13,11 +13,12 @@ Desc:       Programming basics -practise with arithmetic progression etc.
 int apLoop();
 int apLoopEven();
 
-unsigned int randomGenerator();
+int randomGenerator();
 int underHundo();
 int overHundoTouso();
 
-unsigned int multiplication(unsigned int number); // max_value = 2,147,483,647 (2,1 billion)
+int multiplication(int number); // max_value = 2,147,483,647 (2,1 billion)
+
 int collatz(int number);
 int isFibonacci(int x);
 int isPerfectSquare(int x);
@@ -25,7 +26,7 @@ int isPerfectSquare(int x);
 
 int main() {
 
-	unsigned int randomNumber = 0;
+	int randomNumber = 0;
 	int n = 0; // Mathematical integer-variable.
 	
 	int main_iterator = 1;
@@ -33,14 +34,14 @@ int main() {
 	
 
 	printf("A couple of arithmetic progressions:\n#####################################\n");
-    printf("The sum of arithmetic progression of one from 1 to 100 is %d.\n", apLoop());
-    printf("The sum of arithmetic progression of even numbers from 1 to 1000 is %d.\n\n", apLoopEven());
+    printf("The sum of arithmetic progression of one from 1 to 100 (or 10) is %d.\n", apLoop());
+    printf("The sum of arithmetic progression of even numbers from 1 to 1000 (or 10) is %d.\n\n", apLoopEven());
     
-    randomNumber = randomGenerator();
-    printf("Initial random integer: %u\n##########################\n", randomNumber);
+    randomNumber = -3;//randomGenerator();
+    printf("Initial random integer: %d\n##########################\n", randomNumber);
     
     //printf("LONG_MAX : %ld\n", (long) LONG_MAX);
-    printf("After multiplying %u with itself and so on... we got: %u (before overflow)\n\n", randomNumber, multiplication(randomNumber));
+    printf("After multiplying %d with itself and so on... we got: %d (before overflow)\n\n", randomNumber, multiplication(randomNumber));
     
     
     /*
@@ -50,39 +51,50 @@ int main() {
     
     printf("Initial random number for Collatz: %d\n######################################\n", n);
     
-    printf("Collatz sequence for %d is: %d – ", n, n); // Continues from same line (in loop)!
+    if (n == 0 || n < 0) {
+
+        printf("Collatz sequence doesn't make sense for %d! Generating new number...\n\n", n);
+        n = underHundo();
+    }
     
-    while (main_iterator) {
-    	
-    	if (n == 0) {
+    printf("Collatz sequence for %d is: %d – ", n, n); // Continues from same line (in loop)!
+    while (main_iterator) {    	
+
+    	if (n == 0 || n < 0) { // Checking again in case of "bad luck"...
     		
-    		n = underHundo();
-    		printf("Collatz sequence doesn't make sense for 0,\ngenerating new number...\t...%d...\n", n);
+    		printf("Collatz sequence doesn't make sense for %d! Generating new number...\n", n);
+            n = underHundo();
     		printf("Collatz sequence for %d is: %d – ", n, n);
     		
-    	} else {
-    	
+    	} else if (n == 1) {
+            
+            printf("\n");
+            main_iterator = 0;
+
+        } else {
+    	   
+    	    /* Checking the Collatz-math: */
     		if (n % 2 == 0) {
     			
     			n /= 2;
     			
-    			if (n == 1) {
+    			if (n == 1) { // This conditional makes a linebreak at the end of the sequence.
     				
     				rounds++;
-    				printf("%d\n", n);
+    				printf(" %d\n", n);
     				main_iterator = 0;
     				
     			} else {
     				
     				rounds++;
-    				printf("%d – ", n);
+    				printf(" %d –", n); // "_%d_–"
     			}
     		
     		} else {
     			
     			n = n * 3 + 1;
     			rounds++;
-    			printf("%d – ", n);
+    			printf(" %d –", n);
     		}
     		
     	}
@@ -100,7 +112,7 @@ int main() {
     
     	printf("The randomly generated number %d IS a Fibonacci-number.\n", nro);
     	
-    } else if (IsItFibonacci == 0) {
+    } else if (IsItFibonacci == 0 || IsItFibonacci < 0) {
     
     	printf("The randomly generated number %d IS NOT a Fibonacci-number.\n", nro);
     	nro = overHundoTouso();
@@ -148,10 +160,10 @@ int apLoopEven() {
 /*
 Random Number Generators:
 */////////////////////////
-unsigned int randomGenerator() {
+int randomGenerator() {
 	
 	int local_iterator = 1;
-	unsigned int number = 0;
+	int number = 0;
     
 	while (local_iterator) {
 		
@@ -197,27 +209,55 @@ int overHundoTouso() {
 /*
 More math applications:
 *///////////////////////
-unsigned int multiplication(unsigned int number) { // OPERATES WEIRDLY WITH EVEN RNG-INPUTS...
+int multiplication(int number) {
 	
-	//int result = 0;
-	unsigned int uint_max = 4294967295;
+	int rounds = 0;
+	int result = 0; // Using a different variable for actual result (and function return)!
+	int int_max = 2147483647;
+	int int_min = -2147483647;
 	int local_iterator = 1;
 	
-	while (local_iterator) {
+	if (number == 0 || number == 1 || number == -1 || number < 0) {
 	
-		number *= number;
-		printf("Product atm == %u\n", number);
+		printf("Oops, this is not going to work with a %d! Generating a new number...\n", number);
+		number = randomGenerator();
+		printf("New number is => %d\n", number);
+		
+	} else if (number < 0) {
+		
+		while (local_iterator) {
 	
-		if ((number > (uint_max - number))) { // Checks for int overflow BEFORE value_max!
-			local_iterator = 0;
+			number = number * number;
 			
-		} if ((number % 2 == 0)) {
+			if (number < (int_min) || number == 0) { // Checks for int overflow BEFORE value_max!
+				local_iterator = 0;
 			
-			local_iterator = 0;
+			} else {
+			
+				result = number;
+				rounds++;
+				printf("Round %d: %d\n", rounds, result); 
+			}
 		}
 	}
+		
+		while (local_iterator) {
 	
-	return number;
+			number = number * number;
+	
+			/* Using a couple of OR:s to rule out unpredictable behaviour from overflow. */
+			if (number > (int_max) || number < 0 || number == 0) { // Checks for int overflow BEFORE value_max!
+				local_iterator = 0;
+			
+			} else {
+			
+				result = number;
+				rounds++;
+				printf("Round %d: %d\n", rounds, result); 
+			}
+		}
+	
+	return result;
 }
 
 /*
@@ -272,10 +312,22 @@ int isPerfectSquare(int x) {
 	return (s*s == x);
 }
 
+/*
+Based on a mathematical rule about fibonacci-sequence. Then a Logic-operand:
+*/
 int isFibonacci(int x) {
-
-	return isPerfectSquare(5*x*x + 4) || 
-		   isPerfectSquare(5*x*x - 4); // Based on a mathematical rule about fibonacci-sequence. Then a Logic-operand...
+	
+	int result = 0;
+	
+	if (x < 0) {
+		
+		result = isPerfectSquare(5*x*(-x) + 4) || isPerfectSquare(5*x*(-x) - 4);
+	} else {
+	
+		result = isPerfectSquare(5*x*x + 4) || isPerfectSquare(5*x*x - 4);
+	}
+	
+	return result;
 }
 
 
