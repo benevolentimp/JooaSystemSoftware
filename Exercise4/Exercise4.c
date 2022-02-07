934529891f6cc54c630e18f8ce4604a6fb076ec4
benevolentimp <jaakkola.jooa@gmail.com>
2022-02-07 13:04:00 +0200

    Exercise_4 4a

--- Exercise4/Exercise4.c
+++ Exercise4/Exercise4.c
@@ -5,13 +5,18 @@ Desc:       Programming basics - Learning to divide code into "function-blocks"
 */////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int askingNumbers();
 int countNegatives();
 int countSum();
 int countSumPlus();
+int randomNumberGenerator(int first, int second);
 
 int main() {
+
+	int randomNumber = 0;
 	
 	/////////////////////////////////////////////////////////////////
 	/* Exercise_4 3a – Conditions for accurate grammar in output. */
@@ -32,6 +37,7 @@ int main() {
 		printf("You gave %d negative numbers.\n", countedNegatives);
 		printf("\n################################\n\n");
 	}
+	
 	////////////////////
 	/* Exercise_4 3b */
 	int countedSum = countSum();
@@ -44,6 +50,14 @@ int main() {
 	int countedSumPlus = countSumPlus();
 	
 	printf("The sum of positive integers divisible by three is: %d\n", countedSumPlus);
+	printf("\n################################\n\n");
+	
+	/////////////////////////////////////////////////////////////////
+	/* Exercise_4 4a – Loop to generate 2 parameters from console. */
+	
+	randomNumber = randomNumberGenerator(0, 120); // note: CHANGE MANUALLY!
+	
+	printf("Generated 'randomNumber' is: %d\n", randomNumber);
 	printf("\n################################\n\n");
     
     
@@ -62,7 +76,7 @@ int countNegatives() {
 	
 	while (local) {
 		
-		printf("Give an integer (press 0 to exit): "); scanf("%d", &inputs);
+		printf("Give an integer for counting negatives (press 0 to exit): "); scanf("%d", &inputs);
 		
 		if (inputs == 0) {
 			
@@ -89,7 +103,7 @@ int countSum() {
 	
 	while (local) {
 		
-		printf("Give an integer (press 0 to exit): "); scanf("%d", &inputs);
+		printf("Give an integer for summing them (press 0 to exit): "); scanf("%d", &inputs);
 		
 		if (inputs == 0) {
 		
@@ -121,7 +135,7 @@ int countSumPlus() {
 	
 	while (local) {
 		
-		printf("Give an integer (press 0 to exit): "); scanf("%d", &inputs);
+		printf("Give an integer for summing positives divisible by three (press 0 to exit): "); scanf("%d", &inputs);
 		
 		if (inputs == 0) {
 		
@@ -130,11 +144,35 @@ int countSumPlus() {
 			
 		} else if ((inputs > 0) && (inputs % 3 == 0)) {
 		
-			sum += inputs;	
+			sum += inputs;
 		}
 	}
 	
 	return sum;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int randomNumberGenerator(int first, int second) {
+	
+	int randomNumber = 0;
+	
+	srand(time(0));
+		
+	if (first > second) {
+			
+		randomNumber = (rand() % (first - second + 1)) + second;
+			
+	} else if (second > first) {
+			
+		randomNumber = (rand() % (second - first + 1)) + first;
+			
+	} else if (first == second) { // If inputs are equal, no "MIN_RANGE".
+			
+		randomNumber = rand() % (first + 1);
+	}
+		
+	return randomNumber;
 }
 
 /*
