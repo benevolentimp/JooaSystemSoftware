86ff29960e5a3634b7b704b63ebc7b6883d3cedb
benevolentimp <jaakkola.jooa@gmail.com>
2022-02-07 12:00:12 +0200

    Exercise_4 3b

--- Exercise4/Exercise4.c
+++ Exercise4/Exercise4.c
@@ -2,68 +2,115 @@
 Title:      Exercise4.c - Basics of Functions
 Auhtor:     Jooa Jaakkola
 Desc:       Programming basics - Learning to divide code into "function-blocks"
-*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
 
+int askingNumbers();
 int countNegatives();
-
+int countSum();
 
 int main() {
 	
-	int countedNegatives = countNegatives();
+	int countedNegatives = 0;
+	int countedSum = 0;
+	
+	
+	/* Exercise_4 3a – Conditions for accurate grammar in output. */
+	countedNegatives = countNegatives();
 	
 	if (countedNegatives == 1) {
 	
 		printf("You gave %d negative number.\n", countedNegatives);
+		printf("\n################################\n\n");
 		
 	} else if (countedNegatives == 0) {
 	
 		printf("You gave no negative numbers.\n");
+		printf("\n################################\n\n");
 		
 	} else {
 	
 		printf("You gave %d negative numbers.\n", countedNegatives);
+		printf("\n################################\n\n");
 	}
+	
+	
+	/* Exercise_4 3b */
+	countedSum = countSum();
+	
+	printf("The sum of your integers is: %d\n", countedSum);
+	printf("\n################################\n\n");
     
     
     return 0;
 }
 
 /*
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 */
 
 int countNegatives() {
 
+	int local = 1;
 	int inputs = 0;
 	int negatives = 0;
-	int local_iterator = 1;
 	
-	while (local_iterator) {
+	while (local) {
 		
-		printf("Give an integer (Press 0 to exit): "); scanf("%d", &inputs);
+		printf("Give an integer (press 0 to exit): "); scanf("%d", &inputs);
 		
 		if (inputs == 0) {
 			
-			printf("Okay... exiting program...\n\n");
-			local_iterator = 0;
+			printf("Exiting 'countNegatives()'...\n");
+			local = 0;
 			
 		} else if (inputs < 0) {
-			
-			//printf(">>> %d\n", inputs);
+		
 			negatives++;
 			
-		} else {
-			
-			//printf(">>> %d\n", inputs);
 		}
 	}
 	
 	return negatives;
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int countSum() {
+	
+	int local = 1;
+	int inputs = 0;
+	int sum = 0;
+	
+	while (local) {
+		
+		printf("Give an integer (press 0 to exit): "); scanf("%d", &inputs);
+		
+		if (inputs == 0) {
+		
+			printf("Exiting 'countSum()'...\n");
+			sum += inputs;
+			local = 0;
+			
+		} else if (inputs < 0) {
+		
+			sum = sum + inputs;
+			
+		} else {
+		
+			sum += inputs;
+		}
+		// NOTE! If you put {sum += inputs;} here, SUMming happens twice!
+	}
+	
+	return sum;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 /*
-eof
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 */
