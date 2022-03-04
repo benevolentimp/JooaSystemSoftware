26562c2da368fb69babf4da961e2b3d9aa1a2676
benevolentimp <jaakkola.jooa@gmail.com>
2022-03-04 11:21:00 +0200

    Exercise_6 3b

--- Exercise6/Exercise6.c
+++ Exercise6/Exercise6.c
@@ -5,32 +5,86 @@ Description:	Small exercises about pointers and arrays.
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define SIZE 10
 
-void arrayPrint(char *a_pointer, int size);
+void arrayPrint(int *pfAPointer, int size);
+int arrayGenerate(int *eAPointer, int size);
 
 int main() {
 	
-	char array[SIZE] = {'q','w','e','r','t','y','u','i','o','p'};
-	char *arrayPointer = NULL;
-	arrayPointer = &array[0];
+	// Array declos:
+	int prefilledArray[SIZE] = {0,1,2,3,4,5,6,7,8,9};
+	int emptyArray[SIZE];
 	
-	arrayPrint(arrayPointer, SIZE);
+	// Pointer declos:
+	int *pfA = NULL; // == prefilledArray
+	int *eA = NULL; // == emptyArray
+	
+	pfA = &prefilledArray[0];
+	eA = &emptyArray[0];
+	
+	// Function calls:
+	arrayPrint(pfA, SIZE);
+	arrayGenerate(eA, SIZE);
+	arrayPrint(eA, SIZE);
+	
+	// Debugging testing:
+	//printf("%d\n", prefilledArray[0]);
+	
+	printf("%d\n", *eA);
 	
     return 0;
 }
 
-void arrayPrint(char *a_pointer, int size) {
+void arrayPrint(int *pfAPointer, int size) {
 	
 	int i = 0;
 	
 	for (i = 0; i < size; i++) {
 		
-		printf("%c", *a_pointer);
-		a_pointer++;
+		// Special format for beginning of array.
+		if (i == 0) {
+			
+			printf("{%d, ", *pfAPointer);
+			pfAPointer++;
+		
+		// Special format @ the end of array.
+		} else if (i == size-1) {
+			
+			printf("%d}\n", *pfAPointer);
+			
+		} else {
+			
+			printf("%d, ", *pfAPointer);
+			pfAPointer++;
+		}
 	}
-	printf("\n");
+}
+
+int arrayGenerate(int *eAPointer, int size) {
+	
+	int i = 0;
+	int indices = 0;
+	
+	srand(time(0)); // Setting the seed for rand()
+	
+	for (i = 0; i < size; i++) {
+		
+		*eAPointer = rand() % 1000000;
+		indices++;
+		
+		if (indices == size) {
+		
+			i = size;
+		}
+		
+		eAPointer++;
+	}
+	
+	return *eAPointer;
 }
 
 /*
