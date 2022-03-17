3748199f06274780589eed4c09d0f5c92c352f4e
benevolentimp <jaakkola.jooa@gmail.com>
2022-03-17 11:49:28 +0200

    Exercise_7 3a ii)git status

--- Exercise7/Sieve.c
+++ Exercise7/Sieve.c
@@ -27,6 +27,31 @@ int fillMemory(int *ptr, int size) {
 int sieve(int *ptr, int size) {
 	
 	int i = 0;
+	int j = 0;
+	int x = 1;
+	int A[size];
+	
+	// Let A be an array of boolean values set to true (1), indexed 2 to n.
+	for (j = 0; j < size; j++) {
+		A[j] = 1;
+		printf("%d\t", A[j]);
+	} printf("\n\n");
+	
+	/*
+	// for i = 2, 3, 4, ..., not exceeding sqrt(n).
+	for (i = 2; i < sqrt(size); i++) {
+		if (A[i] == 1) {
+			for (j = pow(i, 2); j < size; j + x*i) { // i**2, ... i**2+x*1 not exceeding n.
+				A[j] = 0;
+				x++;
+			}
+		}
+	}
+	
+	for (i = 0; i < size; i++) {
+		printf("%d\n", A[i]);
+	}
+	*/
 	
 	// Iterate every number 2 to size and their multiples. Then write them as 0 into array.
 	for (i = 2; i <= size; i++) {
@@ -59,5 +84,6 @@ int sieve(int *ptr, int size) {
 		}
 	}
 	
+	printf("\n");
 	return *ptr;
 }
