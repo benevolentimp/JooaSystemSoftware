e3b91348f31c6067874147bc50f0cfdc8b8229f6
benevolentimp <jaakkola.jooa@gmail.com>
2022-04-07 15:12:59 +0300

    Makefile working

---
+++ ExerciseWork/Functions.c
@@ -0,0 +1,17 @@
+/**
+ * @file Functions.c
+ * @author Jooa Jaakkola (@benevolentimp)
+ * @brief Implementation for various things.
+ * @version 0.1
+ * @date 2022-04-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+#include "Functions.h"
+
+void function(int n)
+{
+    printf("Hello, W%drld!\n", n);
+}
