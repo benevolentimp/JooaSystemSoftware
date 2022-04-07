c981caedbbaa4cc1561255bf3bbaf85e77de6153
benevolentimp <jaakkola.jooa@gmail.com>
2022-04-07 15:12:19 +0300

    More setup for vscode C-development

---
+++ ExerciseWork/main.c
@@ -0,0 +1,20 @@
+/**
+ * @file main.c
+ * @author Jooa Jaakkola (@benevolentimp)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+
+#include "Functions.h"
+
+int main()
+{
+    int leetNum = 0;
+    function(leetNum);
+    return 0;
+}
