0497326e805a266fa23f1b92d3096b7cfe34fb7b
benevolentimp <jaakkola.jooa@gmail.com>
2022-04-14 22:33:34 +0300

    ExerciseWork Setup

---
+++ ExerciseWork/Main.c
@@ -0,0 +1,14 @@
+/* -¤- coding: utf-8 -¤-
+ * TITLE	: Main.c
+ * AUTHOR	: Jooa Jaakola (@benevolentimp)
+ * DESCRIPTION	: The main implementation program-file.
+*/
+#include "GameSetup.h"
+#include "GameFunction.h"
+
+int main (/*int argc, char *argv[]*/)
+{
+	
+	return 0;
+}
+
